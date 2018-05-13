f72e567655c08a118674e695816aea1223cbcf1a
chirsxjh <1131360171@qq.com>
2018-05-13 17:13:58 +0800

    myproject

---
+++ myproject/app/main/students.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint, render_template, redirect
+
+user = Blueprint('user',__name__)
+
+@user.route('/index')
+def index():
+    return render_template('user/index.html')
+
+@user.route('/add')
+def add():
+    return 'user_add'
+
+@user.route('/show')
+def show():
+    return 'user_show'
